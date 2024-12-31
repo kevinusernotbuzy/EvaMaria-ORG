f2bd5496603f2274dda7cdbfbf39915fdfd6b50a
kevinusernotbuzy <kevinusernotbuzy@gmail.com>
2024-12-31 17:33:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Installing requirements..."
+# Install requirements
 pip3 install -U -r requirements.txt
 
-# Start Flask app for health check
+# Start Flask App
 gunicorn app:app --bind 0.0.0.0:$PORT --daemon
 
-# Start the bot
-echo "Starting Bot..."
+# Start Telegram Bot
 python3 bot.py
