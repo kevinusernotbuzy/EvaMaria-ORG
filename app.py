b5df3aae22e1ab2507765d5a4430319724d5f5d3
kevinusernotbuzy <kevinusernotbuzy@gmail.com>
2025-01-02 10:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     return "Bot is running successfully."
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default port for Koyeb
+    port = int(os.environ.get("PORT", 8080))  # Default port for Koyeb
     app.run(host="0.0.0.0", port=port)
